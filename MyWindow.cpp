7191f005707d8d1b1185e68b18e0619933a68a83
johnnnathan <johnnnathan@proton.me>
2024-11-05 16:57:16 +0100

    Added input validation for binary encoding

--- MyWindow.cpp
+++ MyWindow.cpp
@@ -2,7 +2,7 @@
 #include <QVBoxLayout>
 #include "EncodingHelper.h"
 #include "EncodingType.h"
-
+#include <QRegularExpression>
 MyWindow::MyWindow(QWidget *parent)
     : QWidget(parent)
 {
@@ -26,21 +26,51 @@ MyWindow::MyWindow(QWidget *parent)
 
     setLayout(layout);
 
-    // Connect the editingFinished signal to handleInput slot
-    connect(textField, &QLineEdit::editingFinished, this, &MyWindow::handleInput);
-    // Connect the Encoding Type Dropdown menu for handling
-    connect(encodingTypeDropdown,
-            &QComboBox::currentIndexChanged,
-            this,
-            &MyWindow::handleDropdownChange);
+
+    connect(textField, &QLineEdit::textChanged, this, &MyWindow::handleInput);
+
+    connect(encodingTypeDropdown, &QComboBox::currentIndexChanged, this, &MyWindow::handleDropdownChange);
 }
 
 void MyWindow::handleInput()
 {
-    QString inputText = textField->text().toUpper();
+    QString inputText = textField->text().toUpper().remove(" ");
     EncodingType encoding = getEncodingType(encodingTypeDropdown->currentIndex());
-    int result = encodingToDecimal(encoding, inputText);
-    displayLabel->setText(inputText + "->" + QString::number(result));
+
+
+    bool isValid = false;
+    switch (encoding) {
+    case BINARY:
+        isValid = (inputText.length() % 8 == 0) && (inputText.count('0') + inputText.count('1') == inputText.length());
+        break;
+    case HEX:
+        isValid = (inputText.length() % 2 == 0) && inputText.contains(QRegularExpression("^[0-9A-F]*$")); // Check for valid hex characters
+        break;
+    case DECIMAL:
+        isValid = inputText.toInt();
+        break;
+    case ASCII:
+        isValid = !inputText.isEmpty();
+        break;
+    default:
+        isValid = false;
+        break;
+    }
+
+
+    if (isValid) {
+        textField->setStyleSheet("background-color: green;");
+        if (encoding == BINARY || encoding == HEX) {
+            // Perform conversion if valid
+            int result = encodingToDecimal(encoding, inputText);
+            displayLabel->setText(inputText + " -> " + QString::number(result));
+        } else {
+            displayLabel->setText("Valid " + encodingTypeDropdown->currentText() + " input.");
+        }
+    } else {
+        textField->setStyleSheet("background-color: red;");
+        displayLabel->setText("Invalid input for " + encodingTypeDropdown->currentText());
+    }
 }
 
 void MyWindow::handleDropdownChange(int index)
@@ -48,4 +78,3 @@ void MyWindow::handleDropdownChange(int index)
     QString selectedItem = encodingTypeDropdown->currentText();
     displayLabel->setText("Selected: " + selectedItem);
 }
-
